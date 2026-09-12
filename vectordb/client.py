2d4ba1c357a0a3d5d09a62f6a0c72e77685c4acf
Mckenshel Cameau <mcameau20@gmail.com>
2026-09-11 20:35:32 -0400

    added cosine similary for the retrieval

--- vectordb/client.py
+++ vectordb/client.py
@@ -15,7 +15,11 @@ load_dotenv(os.path.join(VECTORDB_DIR, ".env"))
 
 def get_collection(chroma_path=None):
     client = chromadb.PersistentClient(path=chroma_path or CHROMA_PATH)
-    return client.get_or_create_collection(COLLECTION_NAME)
+    return client.get_or_create_collection(
+        COLLECTION_NAME,
+        metadata={"hnsw:space": "cosine"},
+    )
+
 
 
 def resolve_api_key(api_key=None):
